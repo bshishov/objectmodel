0490177031b7eb41728545ff6330d72a5f6bccd9
Boris Shishov <borisshishov@gmail.com>
2020-06-14 18:02:18 +0300

    unittest -> pytest

--- tests/test_object.py
+++ tests/test_object.py
@@ -1,105 +1,117 @@
-import unittest
-from unittest.mock import MagicMock
+import pytest
 
 from objectmodel import *
 
 
-class ObjectTests(unittest.TestCase):
-    def test_empty_instance(self):
-        class A(ObjectModel):
-            pass
+def test_empty_instance():
+    class A(ObjectModel):
+        pass
 
-        obj = A()
-        self.assertDictEqual(obj.__fields__, {})
-        self.assertDictEqual(obj.__state__, {})
+    obj = A()
+    assert obj.__fields__ == {}
+    assert obj.__state__ == {}
 
-    def test_field_binding(self):
-        class A(ObjectModel):
-            foo = Field()
 
-        instance = A()
-        self.assertTrue('foo' in A.__fields__)
-        self.assertTrue('foo' in instance.__fields__)
-        self.assertTrue('foo' not in instance.__state__)
+def test_field_binding():
+    class A(ObjectModel):
+        foo = Field()
 
-    def test_field_binding_with_custom_name(self):
-        class A(ObjectModel):
-            foo = Field(name='data key')
+    instance = A()
+    assert 'foo' in A.__fields__
+    assert 'foo' in instance.__fields__
+    assert 'foo' not in instance.__state__
 
-        instance = A()
-        self.assertTrue('foo' in A.__fields__)
-        self.assertTrue('foo' in instance.__fields__)
-        self.assertTrue('data key' not in instance.__state__)
 
-    def test_set_field(self):
-        class A(ObjectModel):
-            foo = Field(required=False)
+def test_field_binding_with_custom_name():
+    class A(ObjectModel):
+        foo = Field(name='data key')
 
-        instance = A()
-        value = object()
-        instance.foo = value
-        self.assertEqual(instance.__state__['foo'], value)
-        self.assertEqual(instance.foo, value)
+    instance = A()
+    assert 'foo' in A.__fields__
+    assert 'foo' in instance.__fields__
+    assert 'data key' not in instance.__state__
 
-    def test_access_not_set_attribute_raises(self):
-        class A(ObjectModel):
-            foo = Field()
 
-        instance = A()
+def test_set_field():
+    class A(ObjectModel):
+        foo = Field(required=False)
 
-        with self.assertRaises(AttributeError):
-            value = instance.foo
+    instance = A()
+    value = object()
+    instance.foo = value
+    assert instance.__state__['foo'] == value
+    assert instance.foo == value
 
-    def test_default_value(self):
-        value = object()
 
-        class A(ObjectModel):
-            foo = Field(default=value)
+def test_access_not_set_attribute_raises():
+    class A(ObjectModel):
+        foo = Field()
 
-        instance = A()
-        self.assertTrue('foo' not in instance.__state__)
-        assert instance.foo == value
-        assert 'foo' in instance.__state__
+    instance = A()
 
-    def test_callable_default(self):
-        value = object()
-        factory = MagicMock(return_value=value)
+    with pytest.raises(AttributeError):
+        value = instance.foo
 
-        class A(ObjectModel):
-            foo = Field(default=factory)
 
-        instance = A()
+def test_default_value():
+    value = object()
 
-        assert 'foo' not in instance.__state__
-        assert instance.foo == value
-        factory.assert_called_with()
-        assert 'foo' in instance.__state__
+    class A(ObjectModel):
+        foo = Field(default=value)
 
-    def test_field_inheritance(self):
-        class A(ObjectModel):
-            foo = Field()
+    instance = A()
+    assert 'foo' not in instance.__state__
+    assert instance.foo == value
+    assert 'foo' in instance.__state__
 
-        class B(A):
-            bar = Field()
 
-        assert len(B.__fields__) == 2
-        assert 'foo' in B.__fields__
-        assert 'bar' in B.__fields__
-        assert 'foo' in A.__fields__
-        assert 'bar' not in A.__fields__
+def test_callable_default():
+    value = object()
+    called = False
 
-    def test_kwargs_init(self):
-        class A(ObjectModel):
-            foo = Field()
-            bar = Field()
+    def factory():
+        nonlocal called
+        called = True
+        return value
 
-        obj = A(foo=42, bar='hello')
-        assert obj.foo == 42
-        assert obj.bar == 'hello'
+    class A(ObjectModel):
+        foo = Field(default=factory)
 
-    def test_not_filled_required_field_raises(self):
-        class A(ObjectModel):
-            foo = Field(required=True)
+    instance = A()
 
-        with self.assertRaises(FieldValueRequiredError):
-            A()
+    assert 'foo' not in instance.__state__
+    assert instance.foo == value
+    assert called
+    assert 'foo' in instance.__state__
+
+
+def test_field_inheritance():
+    class A(ObjectModel):
+        foo = Field()
+
+    class B(A):
+        bar = Field()
+
+    assert len(B.__fields__) == 2
+    assert 'foo' in B.__fields__
+    assert 'bar' in B.__fields__
+    assert 'foo' in A.__fields__
+    assert 'bar' not in A.__fields__
+
+
+def test_kwargs_init():
+    class A(ObjectModel):
+        foo = Field()
+        bar = Field()
+
+    obj = A(foo=42, bar='hello')
+    assert obj.foo == 42
+    assert obj.bar == 'hello'
+
+
+def test_not_filled_required_field_raises():
+    class A(ObjectModel):
+        foo = Field(required=True)
+
+    with pytest.raises(FieldValueRequiredError):
+        A()
